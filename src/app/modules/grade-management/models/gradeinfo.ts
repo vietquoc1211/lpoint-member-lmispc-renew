97a9100675a712c34cc8b8eeda41cfcfd4f505f5
Viet Bui <ldcc.vietbui@lotte.vn>
2022-09-19 13:25:39 +0700

    Add grade info models

---
+++ src/app/modules/grade-management/models/gradeinfo.ts
@@ -0,0 +1,13 @@
+export class GradeInfo {
+    asscd?: string;
+    gradecd?: string;
+    gradenm?: string;
+    usegbn?: string;
+    fromcnt?: string;
+    tocnt?: string;
+    ftpsend?: string;
+    insymd?: string;
+    inssabun?: string;
+    lastymd?: string;
+    lastsabun?: string;
+}
