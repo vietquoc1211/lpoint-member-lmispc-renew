d3abbb14904704dae3045f2bc24045de94a4a8ee
Quoc Viet Bui <vietquoc121192@gmail.com>
2022-10-18 17:13:30 +0700

    setting rule

--- src/app/modules/grade-management/services/setting-rule-customer-grade.service.ts
+++ src/app/modules/grade-management/services/setting-rule-customer-grade.service.ts
@@ -1,11 +1,27 @@
 ﻿import { Injectable } from '@angular/core';
 import { ApiService } from '../../../core/_services/api.service';
+import { SettingRuleCustomerGrade } from '../models/setting-rule-customer-grade.model';
 @Injectable({ providedIn: 'root' })
 export class SettingRuleCustomerGradeService {
     constructor(private apiService: ApiService) { }
 
-    getAll() 
-    {
-        return this.apiService.get("SettingRuleCustomerGrade/GetAll");
+    Get() {
+        return this.apiService.get("SettingRuleCustomerGrade");
+    }
+
+    GetByID(asscd: string) {
+        return this.apiService.get(`SettingRuleCustomerGrade/${asscd}`);
+    }
+
+    Post(settingData: SettingRuleCustomerGrade) {
+        return this.apiService.post("SettingRuleCustomerGrade",settingData);
+    }
+
+    Put(asscd: string, settingData: SettingRuleCustomerGrade) {
+        return this.apiService.put(`SettingRuleCustomerGrade/${asscd}`,settingData);
+    }
+
+    Delete(asscd:string) {
+        return this.apiService.delete(`SettingRuleCustomerGrade/${asscd}`);
     }
 }
